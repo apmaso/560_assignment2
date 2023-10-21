8591f41c59dadacd93b1468caa4fd1bd2ed41a8c
Alexander Maso <amaso@mo.ece.pdx.edu>
2023-10-20 17:12:03 -0700

    Added tcl files for FPV, FXP and for SVA inline assertions/assumptions.  Also added bind file and sva file to RTL folder

---
+++ run/aep_sva_run.tcl
@@ -0,0 +1,10 @@
+set_fml_appmode AEP
+set_app_var match_severity_for_builtins true
+read_file -top execution_unit -format sverilog -aep all -sva -vcs {-f ../RTL/filelist}
+
+create_clock clk -period 100 
+create_reset rst_b -sense low
+
+sim_run -stable
+sim_save_reset
+
