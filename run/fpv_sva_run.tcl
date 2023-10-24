658ed64d71607fa95f271df59e4f64fbed8c94b2
Alexander Maso <amaso@mo.ece.pdx.edu>
2023-10-23 20:50:54 -0700

    Added ##1 after implication operator

--- run/fpv_sva_run.tcl
+++ run/fpv_sva_run.tcl
@@ -11,7 +11,7 @@ sim_save_reset
 
 fvassume -expr {@(posedge clk) disable iff (!rst_b)
 	(input_req.req)&&(input_req.req_id) 
-	|-> ((input_req.req_id!=$sampled(input_req.req_id))
+	|-> ##1 ((input_req.req_id!=$sampled(input_req.req_id))
 	until (output_rsp.rsp_id==$sampled(input_req.req_id)))};
 
 
